2d9ce26b59af52e218ee2d6dba14badfb787fe52
Felipe Romao <felipe.romao@unilab.ind.br>
2019-05-16 18:18:43 -0300

    implemetnando cadastro de refrigerantes back end

--- app/Http/Controllers/RefrigeranteController.php
+++ app/Http/Controllers/RefrigeranteController.php
@@ -20,16 +20,6 @@ class RefrigeranteController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -37,7 +27,19 @@ class RefrigeranteController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $valor =        $request->input('valor');
+        $quantidade =   $request->input('quantidade');
+        $marca =        $request->input('marca');
+        $tipo_id =      $request->input('tipo_id');
+        $sabor_id =     $request->input('sabor_id');
+        $litragem_id =  $request->input('litragem_id');
+        $created_at =   date('Y-m-d H:i:s');
+        $updated_at =   date('Y-m-d H:i:s');
+
+        if( !$valor OR !$quantidade OR !$marca OR !$tipo_id OR !$sabor_id OR !$litragem_id ){
+            $response = array('message' => 'Por favor informe os camos obrigatórios');
+            return response($response, 400);
+        }
     }
 
     /**
@@ -48,18 +50,8 @@ class RefrigeranteController extends Controller
      */
     public function show($id)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
+        $refrigerante = Refrigerante::where('id', $id)->first();
+        return response($refrigerante);
     }
 
     /**
@@ -82,6 +74,10 @@ class RefrigeranteController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $deleted = Refrigerante::where('id', $id)->delete();
+        if( $deleted ){
+            $response = array('message' => 'Excluido com sucesso');
+            return response($response);
+        }
     }
 }
